2308825b4108f36bd837c93560274f8f5293b515
amordz72 <dzamor72@gmail.com>
2022-04-27 12:24:10 +0100

    1c

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Abaut page</h1>
+    <NuxtLink to="/about"> About </NuxtLink> <span>|</span>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+<style>
+main span {
+  color: crimson;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about"> About </NuxtLink> <span>|</span>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+<style>
+main span {
+  color: crimson;
+}
+</style>
